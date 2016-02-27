6e445d0fc90158ac0df0e2bfcf183ec76c3225ad
David Victor <david@lionhou.se>
2016-02-26 19:38:32 -0800

    adjusted google analytics settings

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -2,10 +2,22 @@ export function config($logProvider,AnalyticsProvider) {
 	'ngInject';
 	// Enable log
 	$logProvider.debugEnabled(true);
+	// Google analytics
 	AnalyticsProvider
 		.logAllCalls(true)
-		.setAccount('UA-74237418-1')
-		.useECommerce(false, false);
+		.setAccount({
+			tracker: 'UA-74237418-1',
+			name: "portfolio",
+			fields: {
+				cookieDomain: 'techbro.biz',
+				cookieExpires: 86400,
+				siteSpeedSampleRate: 100
+			},
+			displayFeatures: true,
+			enhancedLinkAttribution: true,
+			trackEvent: true,
+			trackEcommerce: false
+		});
 
 
 }
